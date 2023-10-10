80867318a9625385c86608beaaeddf337236a3fc
+ <+>
2023-10-09 22:22:06 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return (["Antonia", "Nuru"]);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return (["Amari", "Mo"]);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiply) {
+    return function(fare) {
+        return fare * multiply;
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+
+function selectDifferentDrivers(arrayOfDrivers, driverSelectionFunction){
+    return driverSelectionFunction(arrayOfDrivers);
+
+}
